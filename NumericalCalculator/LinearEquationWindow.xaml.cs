3ca9565ca4cbe6bac4f27a1382505c1557e636dc
Rychu <Rychu@99afffd6-a438-458e-9746-658195119461>
2012-08-27 22:01:59 +0000

    Już tylko proporcje i główne okno zbudować i będzie można zacząć je programować.
    git-svn-id: svn://nsa210:8777/PierwiastkiCS/trunk@47 99afffd6-a438-458e-9746-658195119461

---
+++ NumericalCalculator/LinearEquationWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NumericalCalculator
+{
+    /// <summary>
+    /// Interaction logic for LinearEquationWindow.xaml
+    /// </summary>
+    public partial class LinearEquationWindow : Window
+    {
+        public LinearEquationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCompute_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
