a54d41e1b211737d4ffd038adaef3b2d5b39ef7d
Rychu <pawel.rychlicki@gmail.com>
2024-03-24 18:32:15 +0100

    Added test framework

---
+++ Tests/FunctionTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rychusoft.NumericalLibraries.Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rychusoft.NumericalLibraries.Tests
+{
+    [TestClass()]
+    public class FunctionTests
+    {
+        [TestMethod()]
+        public void FunctionTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
