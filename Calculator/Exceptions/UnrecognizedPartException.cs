eb7bae8fed726aa9b5dc9094de45be3af5c744f6
Rychu <pawel.rychlicki@gmail.com>
2024-04-14 22:24:14 +0200

    Fixed arcus cotangens and hyperbolic cotangens; Added atan, acot and actn functions; Fixed comma/dot detection; Functions can now be uppercase; Fixed plus operator right after open bracket; Added first integration test

---
+++ Calculator/Exceptions/UnrecognizedPartException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Rychusoft.NumericalLibraries.Calculator.Exceptions
+{
+	public class UnrecognizedPartException(string unrecognizedPart) : Exception($"Unrecognized part: {unrecognizedPart}")
+	{
+        public string UnrecognizedPart { get; set; } = unrecognizedPart;
+    }
+}
